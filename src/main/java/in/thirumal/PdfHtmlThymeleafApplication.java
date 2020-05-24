23988a99de53bb383424cfcf55aaeb6ecc15ce4f
Thirumal <m.thirumal@hotmail.com>
2020-05-24 12:00:31 +0530

    first commit

---
+++ src/main/java/in/thirumal/PdfHtmlThymeleafApplication.java
@@ -0,0 +1,13 @@
+package in.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfHtmlThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfHtmlThymeleafApplication.class, args);
+	}
+
+}
